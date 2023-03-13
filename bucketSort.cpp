1cd38af69ef9783c591b659d09b59a102f323cf9
Juziorex <kontakt@ajozwiak.pl>
2023-03-13 21:08:47 +0100

    new addons

--- bucketSort.cpp
+++ bucketSort.cpp
@@ -6,14 +6,15 @@ using namespace std;
 int main(){
     fstream input,output;
     int howMany = 0;
+    string lines;
 
     input.open("input.txt",ios::in);
     output.open("output.txt",ios::out);
 
-    while(!input.eof()){
+    while(getline(input,lines)){
         howMany++;
-        output << howMany;
     }
+    output << howMany;
     input.close();
     output.close();
 }
