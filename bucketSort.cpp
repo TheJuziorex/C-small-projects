51e6e97e00fb332255f91e24286ee82d46377555
Juziorex <kontakt@ajozwiak.pl>
2023-03-13 21:04:30 +0100

    add a new files "input" and "output"
    I wrote down example numbers in input

--- bucketSort.cpp
+++ bucketSort.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
-    
+    fstream input,output;
+    int howMany = 0;
+
+    input.open("input.txt",ios::in);
+    output.open("output.txt",ios::out);
+
+    while(!input.eof()){
+        howMany++;
+        output << howMany;
+    }
+    input.close();
+    output.close();
 }
