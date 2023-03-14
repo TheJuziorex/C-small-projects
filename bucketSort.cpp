dbcf6ad70c8e3389ad6bba00d6549e41d85a667a
Juziorex <kontakt@ajozwiak.pl>
2023-03-14 22:22:47 +0100

    New sort algorithm with use vectors
    -Vectors
    -More security

--- bucketSort.cpp
+++ bucketSort.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <fstream>
 
-using namespace std;
+void bucketSort(std::vector<int>& nums, int range) {
+    std::vector<std::vector<int>> buckets(range);
 
-int main(){
-    fstream input,output;
-    int howMany = 0;
-    string lines;
-
-    input.open("input.txt",ios::in);
-    output.open("output.txt",ios::out);
-
-    while(getline(input,lines)){
-        howMany++;
+    for (int num : nums) {
+        int bucket_idx = num / range;
+        buckets[bucket_idx].push_back(num);
     }
-    output << howMany;
+
+    for (auto& bucket : buckets) {
+        std::sort(bucket.begin(), bucket.end());
+    }
+
+    int i = 0;
+    for (auto& bucket : buckets) {
+        for (int num : bucket) {
+            nums[i++] = num;
+        }
+    }
+}
+
+int main() {
+    std::fstream input, output;
+    input.open("input.txt", std::ios::in);
+    output.open("output.txt", std::ios::out);
+    if (!input.is_open()) {
+        std::cout << "Nie udalo sie otworzyc pliku wejsciowego.\n";
+        return 1;
+    }
+
+    std::vector<int> nums;
+    int a;
+    while (input >> a) {
+        nums.push_back(a);
+    }
     input.close();
+
+    if (nums.empty()) {
+        std::cout << "Plik wejsciowy jest pusty.\n";
+        return 1;
+    }
+
+    int range = 10;
+    bucketSort(nums, range);
+
+    for (int num : nums) {
+        output << num << " ";
+    }
     output.close();
+
+    return 0;
 }
